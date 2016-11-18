0a900ca9dcca0e77f4c8f1cd856a74487072ad59
Grant Timmerman <granttimmerman@gmail.com>
2016-11-17 21:32:29 -0800

    Fix styles and added world component

---
+++ src/client/world.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+// import Radium from 'radium';
+
+export default class World extends Component {
+  render() {
+    return (
+      <div>
+        world
+      </div>
+    );
+  }
+}
