1beb650078f713482ea9eae4bfba0cfa00413782
Grant Timmerman <granttimmerman@gmail.com>
2016-11-17 21:20:15 -0800

    Add reset styles to method

--- src/client/page.jsx
+++ src/client/page.jsx
@@ -16,35 +16,45 @@ export default class Page extends Component {
         style={[styles.base]}
       >
         <span>helloooooo</span>
-        <Radium.Style rules={{
-          'html, body, div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, menu, nav, output, ruby, section, summary, time, mark, audio, video': {
-            margin: 0,
-            padding: 0,
-            border: 0,
-            fontSize: 0,
-            font: 'inherit',
-            verticalAlign: 'baseline',
-          },
-          'article, aside, details, figcaption, figure, footer, header, hgroup, menu, nav, section': {
-            display: 'block',
-          },
-          'body': {
-            lineHeight: 1,
-          },
-          'ol, ul': {
-            listStyle: 'none',
-          },
-          'blockquote, q': {
-            quotes: 'none',
-          },
-          'blockquote:before, blockquote:after, q:before, q:after': {
-            content: '',
-          },
-          'table': {
-            borderCollapse: 'collapse',
-            borderSpacing: 0,
-          },
-        }}/>
-      </div>);
+        {Page.getResetStyles()}
+      </div>
+    );
+  }
+
+  /**
+   * Gets the reset styles
+   */
+  static getResetStyles() {
+    return (
+      <Radium.Style rules={{
+        'html, body, div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, menu, nav, output, ruby, section, summary, time, mark, audio, video': {
+          margin: 0,
+          padding: 0,
+          border: 0,
+          fontSize: 0,
+          font: 'inherit',
+          verticalAlign: 'baseline',
+        },
+        'article, aside, details, figcaption, figure, footer, header, hgroup, menu, nav, section': {
+          display: 'block',
+        },
+        'body': {
+          lineHeight: 1,
+        },
+        'ol, ul': {
+          listStyle: 'none',
+        },
+        'blockquote, q': {
+          quotes: 'none',
+        },
+        'blockquote:before, blockquote:after, q:before, q:after': {
+          content: '',
+        },
+        'table': {
+          borderCollapse: 'collapse',
+          borderSpacing: 0,
+        },
+      }}/>
+    );
   }
 }
